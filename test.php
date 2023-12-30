953451d404469d44b364d162a82805b35fdd34d7
SolarusXIV-LouisCA <128421398+SolarusXIV-LouisCA@users.noreply.github.com>
2023-12-30 11:28:23 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta https-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Une page PHP 7</title>
+    </head>
+    <body>
+        <?php 
+            echo"<h3> Aujourd'hui me ".date('d / M / Y H:m:s')."</h3><hr />";
+            echo "<h2>Bienvenue sur le Site de PHP 7</h2>";
+        ?>
+    </body>
+</html>
